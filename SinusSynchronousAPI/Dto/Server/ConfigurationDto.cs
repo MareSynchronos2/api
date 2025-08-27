4f9338121e2fb978117e9975871a273340f393c8
Syrilai <30000694+Syrilai@users.noreply.github.com>
2025-08-27 22:47:10 +0200

    feature(Dto): Add ConfigurationDto

---
+++ SinusSynchronousAPI/Dto/Server/ConfigurationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SinusSynchronous.API.Dto.Server;
+
+public record ConfigurationDto
+{
+    public string ServerName { get; set; } = string.Empty;
+    public Version ServerVersion { get; set; } = new(0, 0, 0);
+    public Uri ServerUri { get; set; } = new("wss://noemptyuri");
+    public string? DiscordInvite { get; set; } = null;
+    public string? ServerRules { get; set; } = null;
+}
